f67caa91e2c5a474440ebcb79fe7d04dfef59a4a
Sandra Medina Corey <s.medinacorey@gmail.com>
2016-01-31 17:31:27 -0500

    completed exercises for chapter 1 of Intro to Programming: Ruby

---
+++ basics.rb
@@ -0,0 +1,41 @@
+"sandra" + "medina corey"
+
+ones = 2435 % 10
+tens = 2435 % 100 / 10
+hundreds = 2435 % 1000 / 10
+thousands = 2435 / 1000
+
+movies =  { :beetlejuice => 1988, 
+			:edward_scissorhands => 1990, 
+			:sleepy_hollow => 1999, 
+			:big_fish => 2003, 
+			:coraline => 2009, 
+			:maleficent => 2014	}
+
+puts movies[:beetlejuice]
+puts movies[:edward_scissorhands]
+puts movies[:sleepy_hollow]
+puts movies[:big_fish]
+puts movies[:coraline]
+puts movies[:maleficent]
+
+years = [1988, 1990, 1999, 2003, 2009, 2014]
+
+puts years[0]
+puts years[1]
+puts years[2]
+puts years[3]
+puts years[4]
+puts years[5]
+
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1 
+puts 6 * 5 * 4 * 3 * 2 * 1 
+puts 5 * 4 * 3 * 2 * 1 
+
+puts 3.421 * 3.421
+puts 4.576 * 4.576
+puts 5.324 * 5.324
+
+puts 'There is a missing closing bracket'
+
